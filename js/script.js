b9ae674476cb26480f5af241dd9adf702363c82a
Owusu-Desmond <desmond0406114009@gmail.com>
2022-06-27 14:33:42 +0000

    Include storage

--- js/script.js
+++ js/script.js
@@ -1,54 +1,58 @@
-// array of objects for books
-const books = [
-    {
-        id: 1,
-        title: "The blue bird",
-        author: "Charles Okoye",
-    },
-    {
-        id: 2,
-        title: "Anase in the land of idiot",
-        author: "Desmond Owusu",
-    },
-]
+class Storage {
+    getFromStore = (key) => {
+        if (!localStorage.getItem(key)) return [];
+        else {
+            return JSON.parse(localStorage.getItem(key));
+        }
+    }
+    addToStore = (key, value) => {
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+}
 
+
+const books = new Storage().getFromStore('Books');
 // function to loop through the already added books stored and display them
-const displayBooks = (books) => {
-  books.forEach(book => {
-      const booksContainer = document.querySelector('.added-books-container');
-      const bookContainer = document.createElement('div');
-      const bookContent = `
-                          <p class="book-title"> The title of the book is ${book.title} </p>
-                          <div class= "book-author"> ${book.author} </div>
+const bookContent = (title, author) => {
+    const booksContainer = document.querySelector('.added-books-container');
+    const bookContainer = document.createElement('div');
+    const bookContent = `
+                          <p class="book-title"> The title of the book is ${title} </p>
+                          <div class= "book-author"> ${author} </div>
                           <button type="button"> Remove </button>
                       `;
-      bookContainer.innerHTML = bookContent;
-      booksContainer.appendChild(bookContainer);
-  })
+    bookContainer.innerHTML = bookContent;
+    booksContainer.appendChild(bookContainer);
+}
+const displayBooks = (books) => {
+    books.forEach(book => {
+        bookContent(book.title, book.author)
+    })
 }
 displayBooks(books);
 
 const form = document.getElementById('form');
 form.addEventListener('submit', ((event) => {
-  event.preventDefault();
-  addBooks(books);
+    event.preventDefault();
+    addBooks(books);
 })
 )
 
 // Fuction to add books to array
 const addBooks = (books) => {
- 
-  const title = form.elements.title.value
-  const author = form.elements.author.value
+    const title = form.elements.title.value
+    const author = form.elements.author.value
 
-  let object = {
-    id : books.length + 1,
-    title : title,
-    author : author
-  }
-  books.push(object);
-  alert(title)
-  displayBooks(books);
+    let book = {
+        id: books.length + 1,
+        title: title,
+        author: author
+    }
+    books.push(book);
+    // add book to storage when from onsubmit
+    new Storage().addToStore("Books", books);
+    // add to user interface when form onsubmit
+    bookContent(title, author);
 }
 
 
