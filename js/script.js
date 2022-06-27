7f14a8475e43f016476896b158e8cbbc0d0778ca
Owusu-Desmond <desmond0406114009@gmail.com>
2022-06-27 16:43:27 +0000

    Remove Book from storage

--- js/script.js
+++ js/script.js
@@ -8,30 +8,40 @@ class Storage {
     addToStore = (key, value) => {
         localStorage.setItem(key, JSON.stringify(value))
     }
-    // deleteFromStore = (key, value, id) => {
-    //     localStorage.setItem(key, JSON.stringify(value))
-    // }
+    deleteFromStore = (key, title) => {
+        // first get the books from storage
+        let books = this.getFromStore(key);
+        // remove book base on title
+        books.forEach((book, index) => {
+            if (book.title === title) {
+                books.splice(index, 1)
+            }
+        })
+        // add remaining books to storage
+        this.addToStore(key, books)
+    }
 }
 
 let storage = new Storage()
 
 const books = storage.getFromStore('Books');
 // function to loop through the already added books stored and display them
-const bookContent = (id, title, author) => {
+const bookContent = (title, author) => {
     const booksContainer = document.querySelector('.added-books-container');
     const bookContainer = document.createElement('div');
-    bookContainer.setAttribute('id', id)
+    bookContainer.setAttribute('id', `bookContainer${title}`)
     const bookContent = `
                           <p class="book-title"> The title of the book is ${title} </p>
                           <div class= "book-author"> ${author} </div>
-                          <button type="button" class="remove-button"> Remove </button>
+                          <button id = '${title}' type="button" class="remove-button"> Remove </button>
+                          <hr>
                       `;
     bookContainer.innerHTML = bookContent;
     booksContainer.appendChild(bookContainer);
 }
 const displayBooks = (books) => {
     books.forEach(book => {
-        bookContent(book.id, book.title, book.author)
+        bookContent(book.title, book.author)
     })
 }
 displayBooks(books);
@@ -47,9 +57,7 @@ form.addEventListener('submit', ((event) => {
 const addBooks = (books) => {
     const title = form.elements.title.value
     const author = form.elements.author.value
-    const id =  books.length + 1
     let book = {
-        id: id,
         title: title,
         author: author
     }
@@ -57,14 +65,15 @@ const addBooks = (books) => {
     // add book to storage when from onsubmit
     new Storage().addToStore("Books", books);
     // add to user interface when form onsubmit
-    bookContent(id, title, author);
+    bookContent(title, author);
 }
 
 const removeButtons = Array.from(document.querySelectorAll('.remove-button'))
 
 removeButtons.forEach(button => {
   button.addEventListener('click', () => {
-    removeBook(books, button.parentElement.id)
+    storage.deleteFromStore('Books', button.id);
+    document.getElementById(`bookContainer${button.id}`).style.display = 'none';
   })
 })
 
