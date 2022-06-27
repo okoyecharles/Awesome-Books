50f879ef7310a8c260534a909cd1b16cb7b429bc
OkoyeCharles <okoyecharles509@gmail.com>
2022-06-27 14:36:17 +0100

    Add Javascript function to add books

--- js/script.js
+++ js/script.js
@@ -11,18 +11,44 @@ const books = [
         author: "Desmond Owusu",
     },
 ]
+
 // function to loop through the already added books stored and display them
 const displayBooks = (books) => {
-    books.forEach(book => {
-        const booksContainer = document.querySelector('.added-books-container');
-        const bookContainer = document.createElement('div');
-        const bookContent = `
-                            <p class="book-title"> The title of the book is ${book.title} </p>
-                            <div class= "book-author"> ${book.author} </div>
-                            <button type="button"> Remove </button>
-                        `;
-        bookContainer.innerHTML = bookContent;
-        booksContainer.appendChild(bookContainer);
-    })
+  books.forEach(book => {
+      const booksContainer = document.querySelector('.added-books-container');
+      const bookContainer = document.createElement('div');
+      const bookContent = `
+                          <p class="book-title"> The title of the book is ${book.title} </p>
+                          <div class= "book-author"> ${book.author} </div>
+                          <button type="button"> Remove </button>
+                      `;
+      bookContainer.innerHTML = bookContent;
+      booksContainer.appendChild(bookContainer);
+  })
 }
 displayBooks(books);
+
+const form = document.getElementById('form');
+form.addEventListener('submit', ((event) => {
+  event.preventDefault();
+  addBooks(books);
+})
+)
+
+// Fuction to add books to array
+const addBooks = (books) => {
+ 
+  const title = form.elements.title.value
+  const author = form.elements.author.value
+
+  let object = {
+    id : books.length + 1,
+    title : title,
+    author : author
+  }
+  books.push(object);
+  alert(title)
+  displayBooks(books);
+}
+
+
