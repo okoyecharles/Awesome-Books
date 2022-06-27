f6ec62cf6a0f02027644ed8d56edd3a5240b3685
OkoyeCharles <okoyecharles509@gmail.com>
2022-06-27 16:13:20 +0100

    Add remove button classes

--- js/script.js
+++ js/script.js
@@ -8,25 +8,30 @@ class Storage {
     addToStore = (key, value) => {
         localStorage.setItem(key, JSON.stringify(value))
     }
+    // deleteFromStore = (key, value, id) => {
+    //     localStorage.setItem(key, JSON.stringify(value))
+    // }
 }
 
+let storage = new Storage()
 
-const books = new Storage().getFromStore('Books');
+const books = storage.getFromStore('Books');
 // function to loop through the already added books stored and display them
-const bookContent = (title, author) => {
+const bookContent = (id, title, author) => {
     const booksContainer = document.querySelector('.added-books-container');
     const bookContainer = document.createElement('div');
+    bookContainer.setAttribute('id', id)
     const bookContent = `
                           <p class="book-title"> The title of the book is ${title} </p>
                           <div class= "book-author"> ${author} </div>
-                          <button type="button"> Remove </button>
+                          <button type="button" class="remove-button"> Remove </button>
                       `;
     bookContainer.innerHTML = bookContent;
     booksContainer.appendChild(bookContainer);
 }
 const displayBooks = (books) => {
     books.forEach(book => {
-        bookContent(book.title, book.author)
+        bookContent(book.id, book.title, book.author)
     })
 }
 displayBooks(books);
@@ -42,9 +47,9 @@ form.addEventListener('submit', ((event) => {
 const addBooks = (books) => {
     const title = form.elements.title.value
     const author = form.elements.author.value
-
+    const id =  books.length + 1
     let book = {
-        id: books.length + 1,
+        id: id,
         title: title,
         author: author
     }
@@ -52,7 +57,15 @@ const addBooks = (books) => {
     // add book to storage when from onsubmit
     new Storage().addToStore("Books", books);
     // add to user interface when form onsubmit
-    bookContent(title, author);
+    bookContent(id, title, author);
 }
+
+const removeButtons = Array.from(document.querySelectorAll('.remove-button'))
+
+removeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    removeBook(books, button.parentElement.id)
+  })
+})
 
 
