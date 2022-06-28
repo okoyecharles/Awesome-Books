217e1c9d7da0ed6e128852f3af0fe1bd71f50be7
OkoyeCharles <okoyecharles509@gmail.com>
2022-06-28 13:58:09 +0100

    Fix Linter Errors

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// ====== These are the elments that will be altered universally
+const form = document.getElementById('form');
+const booksContainer = document.querySelector('.added-books-container');
+
 // ====== Book Class : Creates a Book when Instantiated ======
 class Book {
   constructor(title, author) {
@@ -6,55 +10,19 @@ class Book {
   }
 }
 
-// ====== UI Class : Updates Specific parts of the User Interface ======
-class UI {
-  static updateBooks(booksContainer) {
-    // Get Books from store and add them to UI
-    const books = Store.getBooks()
-    booksContainer.innerHTML = ``
-    if(books){
-      books.forEach(book => this.addBook(book))
-    }
-    // Remove Books from UI when 'remove' button is clicked
-    const btns = Array.from(document.querySelectorAll('.remove-button'))
-    btns.forEach(btn => btn.addEventListener('click', () => {
-      UI.removeBook(btn)
-    }))
-  }
-
-  static addBook(book) {
-    const bookUI = document.createElement('div')
-    bookUI.className = 'book'
-    bookUI.innerHTML = `
-    <p class="book-title">"${book.title}" by ${book.author}</p>
-    <button type="button" class="remove-button"><i class="fa-solid fa-xmark"></i></button>`  
-    booksContainer.append(bookUI)
-  }
-
-  static clearInputs() {
-    form.elements.title.value = '';
-    form.elements.author.value = '';
-  }
-
-  static removeBook(btn) {
-    btn.parentElement.remove()
-    Store.removeBook(btn)
-  }
-}
-
 // ====== Store Class: Handles all Operations on Local Storage ======
 class Store {
   static addBook(book) {
-    const books = JSON.parse(localStorage.getItem('books'))
+    const books = JSON.parse(localStorage.getItem('books'));
     // Check if books exist then add them to Local Storage
     if (!books) {
       const books = [];
-      localStorage.setItem('books', JSON.stringify(books))     
+      localStorage.setItem('books', JSON.stringify(books));
       books.push(book);
-      localStorage.setItem('books', JSON.stringify(books))
+      localStorage.setItem('books', JSON.stringify(books));
     } else {
       books.push(book);
-      localStorage.setItem('books', JSON.stringify(books))
+      localStorage.setItem('books', JSON.stringify(books));
     }
   }
 
@@ -67,28 +35,63 @@ class Store {
   static removeBook(btn) {
     // Remove books from storage
     let books = Store.getBooks();
-    books = books.filter(book => !btn.parentElement.children[0].innerHTML.includes(book.title && book.author));
+    const p = btn.parentElement.children[0].innerHTML;
+    books = books.filter((book) => !p.includes(book.title && book.author));
     localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
+// ====== UI Class : Updates Specific parts of the User Interface ======
+class UI {
+  static updateBooks(booksContainer) {
+    // Get Books from store and add them to UI
+    const books = Store.getBooks();
+    booksContainer.innerHTML = '';
+    if (books) {
+      books.forEach((book) => this.addBook(book));
+    }
+    // Remove Books from UI when 'remove' button is clicked
+    const btns = Array.from(document.querySelectorAll('.remove-button'));
+    btns.forEach((btn) => btn.addEventListener('click', () => {
+      UI.removeBook(btn);
+    }));
+  }
+
+  static addBook(book) {
+    const bookUI = document.createElement('div');
+    bookUI.className = 'book';
+    bookUI.innerHTML = `
+    <p class="book-title">"${book.title}" by ${book.author}</p>
+    <button type="button" class="remove-button"><i class="fa-solid fa-xmark"></i></button>`;
+    booksContainer.append(bookUI);
+  }
+
+  static clearInputs() {
+    form.elements.title.value = '';
+    form.elements.author.value = '';
+  }
+
+  static removeBook(btn) {
+    btn.parentElement.remove();
+    Store.removeBook(btn);
+  }
+}
+
 // ====== Form Function : Listens for Form Submission then executes Functions ======
-const form = document.getElementById('form');
-const booksContainer = document.querySelector('.added-books-container')
 form.addEventListener('submit', (event) => {
-  event.preventDefault()
+  event.preventDefault();
   const title = form.elements.title.value;
   const author = form.elements.author.value;
   // Test String for letters
   const validate = /[a-zA-Z]/g;
   if (validate.test(title) && validate.test(author)) {
-    const book = new Book(title, author)
-    Store.addBook(book)
-    UI.addBook(book)
-    UI.updateBooks(booksContainer)
-    UI.clearInputs()
-  } 
-})
+    const book = new Book(title, author);
+    Store.addBook(book);
+    UI.addBook(book);
+    UI.updateBooks(booksContainer);
+    UI.clearInputs();
+  }
+});
 
 // Display Books When Page is reloaded
-UI.updateBooks(document.querySelector('.added-books-container'))
+UI.updateBooks(document.querySelector('.added-books-container'));
